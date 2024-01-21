953d47acf7161c965f1374030d1b4f23be9ad655
Hamza Ahmad <106329588+Hamza-Ahmad2170@users.noreply.github.com>
2024-01-22 02:07:15 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/03flash_card/',
+  base: '/Challenge_01_flash_card/',
   plugins: [react()],
 })
